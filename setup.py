8f7a192c71f9bf2d01c161adf4a950feeeee952e
Jef Oliver <jef@eljef.me>
2021-03-15 17:06:20 -0700

    Version 1.5.0
    Signed-off-by: Jef Oliver <jef@eljef.me>

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setup(
     packages=['eljef.core'],
     python_requires='>=3.8',
     url='https://github.com/eljef/python_eljef_core',
-    version='1.4.2',
+    version='1.5.0',
     zip_safe=False,
 )
